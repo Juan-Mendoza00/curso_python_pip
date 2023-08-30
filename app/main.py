6b9f8b874549a2e3aa83fa2322abb3ef909b5af3
Juancml <camilo.mendoza0416@gmail.com>
2023-08-30 11:45:39 -0500

    App Requirements updated. More tests with app/main.py now using pandas to read csv file

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
-  data = read_csv.read_csv('./data.csv')
+  # data = read_csv.read_csv('./data.csv')
+  data  = pd.read_csv('./data.csv')
 
   country = input('Type country name: ')
-  
+
+  '''
   country_dict = utils.get_country(country, data)
   country_ft = utils.get_feature(country_dict, 'population')
 
+  '''
 
-  if len(country_ft) > 0:
-    charts.bar_chart(country_ft.keys(),country_ft.values(),country_dict['Country/Territory'])
+  # To get a row whith pandas and specified country name
+  country_row = data.loc[lambda df: df['Country/Territory'] == country.title()]
+  population = country_row[[ft for ft in country_row.columns if ft.endswith('Population')]]
+
+  if len(population) > 0:
+    labels = population.columns.values
+    ppl = [float(value) for value in population.values[0]]
+    charts.bar_chart(labels,ppl,country_row['Country/Territory'].values[0])
 
 #to run main as a script (when called in terminal)
 if __name__ == '__main__':
